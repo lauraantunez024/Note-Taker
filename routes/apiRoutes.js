040845c2f5b597fee0f577068b53105be299cc03
Laura Antunez <lauraantunez024@gmail.com>
2022-10-11 19:31:19 -0400

    Fixed previous bugs. New notes can be saved and deleted

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,29 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid')
 
 const {
     readFromFile,
     readAndAppend,
-    writeToFile,
+    readAndDelete,
   } = require('../helpers/fsUtils');
   
 
-router.get('/notes', (req, res) => {
-    res.json('get')
-})
+  router.get("/notes", (req, res) => {
+    readFromFile("./db/db.json").
+    then((data) => res.json(JSON.parse(data)));
+  });
+
 
 router.post('/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4();
-    readAndAppend(newNote, './db/db.json')
+    readAndAppend(newNote, './db/db.json');
+    res.json(newNote);
 });
+
+router.delete("/notes/:id", (req, res) => {
+    readAndDelete(req.params.id, "./db/db.json");
+    res.json({ ok: true });
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require("express");
-// const req = require("express/lib/request");
-// const router = require("express").Router();
+const app = express();
+
+
 const htmlRoutes = require('./routes/htmlRoutes')
 const apiRoutes = require('./routes/apiRoutes')
 
 
-const app = express();
 const PORT = process.env.PORT || 3001; 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
-// API ROUTES
-app.post('/api/notes', (req, res) => {
-    res.json('post')
 
-})
 
 app.use('/api', apiRoutes);
 
@@ -26,3 +21,4 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
